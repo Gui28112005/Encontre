9959d492faef0a07050b42bb1565e4c4a759eb5a
Guilherme Lopes Rocetom <158601578+Gui28112005@users.noreply.github.com>
2024-10-20 19:14:51 -0300

    Update script.js

--- encontre web 2/js/script.js
+++ encontre web 2/js/script.js
@@ -5,7 +5,7 @@ let loading = true;
 const carregarComercios = async () => {
     loading = true;
     try {
-        const response = await fetch('https://backendecontre2.azurewebsites.net/comercio');
+        const response = await fetch('https://backendencontre01.azurewebsites.net/comercio');
         const data = await response.json();
         comercios = data;
         filteredComercios = data;
@@ -16,6 +16,10 @@ const carregarComercios = async () => {
         setSelectOptions('estado', estadosUnicos, 'Selecione o Estado');
         setSelectOptions('cidade', cidadesUnicas, 'Selecione a Cidade');
         exibirComercios(filteredComercios);
+
+        document.getElementById('estado').addEventListener('change', atualizarCidades);
+        document.getElementById('categoria').addEventListener('change', filtrarComercios);
+        document.getElementById('cidade').addEventListener('change', filtrarComercios);
     } catch (error) {
         console.error(error);
     } finally {
@@ -23,17 +27,27 @@ const carregarComercios = async () => {
     }
 };
 
+const atualizarCidades = () => {
+    const selectedEstado = document.getElementById('estado').value;
+    const cidadesFiltradas = [...new Set(comercios
+        .filter(comercio => comercio.estado === selectedEstado)
+        .map(comercio => comercio.cidade))];
+
+    setSelectOptions('cidade', cidadesFiltradas, 'Selecione a Cidade');
+
+    // Atualiza comercios ao mudar o estado
+    filtrarComercios();
+};
+
 const setSelectOptions = (selectId, options, defaultText) => {
     const select = document.getElementById(selectId);
-    if (select) {
-        select.innerHTML = `<option value="">${defaultText}</option>`; // Limpa opções anteriores
-        options.forEach(option => {
-            const opt = document.createElement('option');
-            opt.value = option;
-            opt.textContent = option;
-            select.appendChild(opt);
-        });
-    }
+    select.innerHTML = `<option value="">${defaultText}</option>`; // Limpa opções anteriores
+    options.forEach(option => {
+        const opt = document.createElement('option');
+        opt.value = option;
+        opt.textContent = option;
+        select.appendChild(opt);
+    });
 };
 
 const filtrarComercios = () => {
@@ -52,8 +66,14 @@ const filtrarComercios = () => {
 
 const exibirComercios = (comerciosParaExibir) => {
     const container = document.getElementById('comerciosContainer');
-    if (container) {
-        container.innerHTML = ''; // Limpa o container
+    container.innerHTML = ''; // Limpa o container
+
+    if (comerciosParaExibir.length === 0) {
+        const mensagemDiv = document.createElement('div');
+        mensagemDiv.className = 'mensagem';
+        mensagemDiv.innerHTML = `<p>Em Breve Novos Lugares estarão aqui! Volte Mais Tarde.</p>`;
+        container.appendChild(mensagemDiv);
+    } else {
         comerciosParaExibir.forEach(comercio => {
             const comercioDiv = document.createElement('div');
             comercioDiv.className = 'comercio-item';
@@ -63,15 +83,27 @@ const exibirComercios = (comerciosParaExibir) => {
                     <h3>${comercio.nome}</h3>
                 </div>
                 <div class="redes-sociais-container">
-                    <h2>De onde Somos?</h2>
+                    <h2 class="titulo-com-icone">
+                        <img src="./img/icons8-usuário-homem-com-círculo-50.png" alt="Contato" class="icon" /> Contato:
+                    </h2>
                     <div class="info-container">
                         <p class="info-item">Cidade: ${comercio.cidade || 'Não disponível'}</p>
                         <p class="info-item">Estado: ${comercio.estado || 'Não disponível'}</p>
                         <p class="info-item">Telefone: ${comercio.telefone || 'Não disponível'}</p>
+                        <p class="info-item">Horário de Funcionamento: ${comercio.horario_funcionamento || 'Não disponível'}</p>
+                        <p class="info-item">Movimento Maior: ${comercio.horario_funcionamento_feriados || 'Não disponível'}</p>
                     </div>
                 </div>
+                <div class="descricao-container">
+                    <h2 class="titulo-com-icone">
+                        <img src="./img/icons8-contato-comercial-50.png" alt="Sobre Nós" class="icon" /> Sobre Nós:
+                    </h2>
+                    <p class="description">${comercio.descricao || 'Descrição não disponível'}</p>
+                </div>
                 <div class="redes-sociais-container">
-                    <h2>Nossas Redes Sociais:</h2>
+                    <h2 class="titulo-com-icone">
+                        <img src="./img/icons8-avião-de-papel-50.png" alt="Redes Sociais" class="icon" /> Redes Sociais:
+                    </h2>
                     <div class="info-container">
                         <div class="links-container">
                             <div class="icon-container">
@@ -83,15 +115,10 @@ const exibirComercios = (comerciosParaExibir) => {
                                 <a href="${comercio.link_facebook || '#'}" target="_blank">
                                     <img src="./img/icons8-facebook-novo-50.png" alt="Facebook" class="icon" />
                                 </a>
-                            </div> 
+                            </div>
                             <div class="icon-container">
                                 <a href="${comercio.link_instagram || '#'}" target="_blank">
                                     <img src="./img/icons8-instagram-48.png" alt="Instagram" class="icon" />
-                                </a>
-                            </div>
-                            <div class="icon-container">
-                                <a href="${comercio.telefone ? 'https://api.whatsapp.com/send?phone=' + comercio.telefone.replace(/\D/g, '') : '#'}" target="_blank">
-                                    <img src="./img/icons8-whatsapp-50.png" alt="WhatsApp" class="icon" />
                                 </a>
                             </div>
                             <div class="icon-container">
@@ -99,12 +126,13 @@ const exibirComercios = (comerciosParaExibir) => {
                                     <img src="./img/icons8-google-maps-novo-48.png" alt="Maps" class="icon" />
                                 </a>
                             </div>
+                            <div class="icon-container">
+                                <a href="${comercio.link_site_pessoal || '#'}" target="_blank">
+                                    <img src="./img/icons8-internet-50.png" alt="Site Pessoal" class="icon" />
+                                </a>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="descricao-container">
-                    <h2>Sobre Nós:</h2>
-                    <p class="description">${comercio.descricao || 'Descrição não disponível'}</p>
                 </div>
             `;
             container.appendChild(comercioDiv);
@@ -112,35 +140,5 @@ const exibirComercios = (comerciosParaExibir) => {
     }
 };
 
-window.onload = function() {
-    carregarComercios();
-    // Verificação de cookies
-    if (document.cookie.indexOf("cookies_accepted=true") === -1) {
-        setTimeout(function() {
-            const cookieConsent = document.getElementById('cookie-consent');
-            if (cookieConsent) {
-                cookieConsent.style.display = 'flex';
-            }
-        }, 1000);
-    } else {
-        const consentMessage = document.getElementById('consent-message');
-        if (consentMessage) {
-            consentMessage.style.display = 'block';
-        }
-    }
-};
 
-function acceptCookies() {
-    var d = new Date();
-    d.setTime(d.getTime() + (30*24*60*60*1000)); // 30 dias
-    var expires = "expires=" + d.toUTCString();
-    document.cookie = "cookies_accepted=true;" + expires + ";path=/";
-    const cookieConsent = document.getElementById('cookie-consent');
-    const consentMessage = document.getElementById('consent-message');
-    if (cookieConsent) {
-        cookieConsent.style.display = 'none';
-    }
-    if (consentMessage) {
-        consentMessage.style.display = 'block';
-    }
-}
+window.onload = carregarComercios;
